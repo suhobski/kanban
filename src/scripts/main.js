b2058bb9bbc42cd0a286b3db1060520a9e039400
suhobski <suhobski@mail.ru>
2020-12-04 14:58:10 +0300

    feat: task styles

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+// раскрытие комментария при нажатии на кнопку. Вешаем событие на все кнопки
+let taskButtons = document.querySelectorAll('.task__button--full-comment')
+
+taskButtons.forEach(taskButton => {
+  taskButton.addEventListener('click', (event) => {
+    let comment = event.target.parentNode
+    comment.style.height = 'auto'
+    event.target.style.display = 'none'
+  })
+})
