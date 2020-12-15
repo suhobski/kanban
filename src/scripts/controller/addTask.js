2db4b46657a12154b1ca2b147cd8534df19bd239
suhobski <suhobski@mail.ru>
2020-12-15 15:45:17 +0300

    feat: add save task in localStorage

--- src/scripts/controller/addTask.js
+++ src/scripts/controller/addTask.js
@@ -4,9 +4,6 @@ let modalTitle = document.querySelector('.modal-add-task__field--title')
 let modalDate = document.querySelector('.modal-add-task__field--date')
 let modalUser = document.querySelector('.modal-add-task__field--user-list')
 let modalComment = document.querySelector('.modal-add-task__field--comment')
-
-
-
 let buttonOk = document.querySelector('.modal-add-task__button--ok')
 
 export function addTask() {
@@ -17,5 +14,14 @@ export function addTask() {
     let taskComment = {name: taskUser, comment: modalComment.value}
     
     let task = new Task(taskTitle, taskUser, taskDate, taskComment)
+
+    // проверяем наличие 'todo' в localStorage
+    if (localStorage.getItem('todo')) {
+      let todoArray = JSON.parse(localStorage.getItem('todo'))  // если есть, сохраняем себе
+      todoArray.push(task)                                      // добавялем новую задачу
+      localStorage.setItem('todo', JSON.stringify(todoArray))   // сохраняем обратно
+    } else {
+      localStorage.setItem('todo', JSON.stringify([task]))      // иначе просто сохраняем 
+    }
   })
 }
