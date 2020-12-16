787b34694cf97697805609a0dd282cab6769514b
suhobski <suhobski@mail.ru>
2020-12-16 14:40:01 +0300

    fix: delete menu

--- src/scripts/view/view.js
+++ src/scripts/view/view.js
@@ -1,14 +1,10 @@
 import { hideLongComment,displayFullComment } from './comment.js'
-import { viewMenu } from './taskMenu.js'
 import { displayModalAddTask, closeModalAddTaskListeners, createListOfUsers } from './modalAddTask.js'
 
 export function view() {
   // комментарий задачи
   hideLongComment()
   displayFullComment()
-
-  // меню задачи
-  viewMenu()
 
   // модальное окно "Add task"
   displayModalAddTask()
